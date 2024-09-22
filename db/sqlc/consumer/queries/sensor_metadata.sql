279dc8de04603b721081376f4f23db7a04d59683
christoff-linde <christoff.linde@gmail.com>
2024-09-22 19:34:35 +0200

    feat(db): add GetSensorMetadataForSensorID query

--- db/sqlc/consumer/queries/sensor_metadata.sql
+++ db/sqlc/consumer/queries/sensor_metadata.sql
@@ -1,4 +1,11 @@
 -- name: CreateSensorMetadata :one
-INSERT INTO sensor_metadata ( id, sensor_id, manufacturer, model_number, installation_time, updated_at, additional_data )
-VALUES ($1, $2, $3, $4, $5, $6, $7)
+INSERT INTO sensor_metadata ( id, sensor_id, manufacturer, model_number,  additional_data )
+VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetSensorMetadataForSensorId :one
+SELECT sensors.*
+FROM sensors
+         JOIN sensor_metadata ON sensors.id = sensor_metadata.sensor_id
+WHERE sensor_metadata.sensor_id = $1
+LIMIT $2;
