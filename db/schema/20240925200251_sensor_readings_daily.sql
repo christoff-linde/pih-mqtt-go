4aa0dffd31315f337ec4ac2cba2f38ebdfaf320e
christoff-linde <christoff.linde@gmail.com>
2024-09-25 23:00:20 +0200

    feat(db): add sensor_readings_daily materialized view

---
+++ db/schema/20240925200251_sensor_readings_daily.sql
@@ -0,0 +1,22 @@
+-- +goose NO TRANSACTION
+-- +goose Up
+-- +goose StatementBegin
+CREATE MATERIALIZED VIEW IF NOT EXISTS sensor_readings_daily
+WITH (timescaledb.continuous) AS
+SELECT 
+  time_bucket('1 day', sensor_readings.reading_timestamp) AS day,
+  sensor_id,
+  min(temperature) as min_temperature,
+  avg(temperature) as avg_temperature,
+  max(temperature) as max_temperature,
+  min(humidity) as min_humidity,
+  avg(humidity) as avg_humidity,
+  max(humidity) as max_humidity
+FROM sensor_readings
+GROUP BY day, sensor_id;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP MATERIALIZED VIEW sensor_readings_daily;
+-- +goose StatementEnd
