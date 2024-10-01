015ded8cf5eca8c83163cdcb0922ca1f3f224920
christoff-linde <christoff.linde@gmail.com>
2024-10-01 21:39:36 +0200

    feat(api): implement GET sensor operations
    this includes GetSensors and GetSensorById routes

--- api/internal/server/routes.go
+++ api/internal/server/routes.go
@@ -3,7 +3,9 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	db "github.com/christoff-linde/pih-core-go/api/database"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,36 +16,53 @@ func (s *Server) RegisterRoutes() http.Handler {
 	sensors := v1.Group("/sensors")
 	{
 		sensors.GET("/", s.GetSensorsHandler)
-		sensors.GET("/:sensor_id", s.GetSensorByIdHandler)
+		sensors.GET("/:id", s.GetSensorByIdHandler)
 		sensors.POST("/", s.CreateSensorHandler)
-		sensors.PUT("/:sensor_id", s.UpdateSensorHandler)
-		sensors.DELETE("/:sensor_id", s.DeleteSensorHandler)
+		sensors.PUT("/:id", s.UpdateSensorHandler)
+		sensors.DELETE("/:id", s.DeleteSensorHandler)
 	}
 	return r
 }
 
 func (s *Server) GetSensorsHandler(c *gin.Context) {
-	limit := c.DefaultQuery("limit", "20")
-	offset := c.DefaultQuery("offset", "0")
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "20"))
+	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
+	nextOffset := offset + limit
 
-	resp := make(map[string]string)
-	resp["limit"] = limit
-	resp["offset"] = offset
-	resp["message"] = "Yeetus Deletus"
+	sensors, err := s.db.GetSensors(c, db.GetSensorsParams{
+		Limit:  int32(limit),
+		Offset: int32(offset),
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	if sensors == nil {
+		nextOffset = 0
+	}
 
+	resp := gin.H{
+		"message":   fmt.Sprintf("Found %d sensors", len(sensors)),
+		"data":      sensors,
+		"next_page": nextOffset,
+	}
 	c.JSON(http.StatusOK, resp)
 }
 
 func (s *Server) GetSensorByIdHandler(c *gin.Context) {
-	id := c.Query("sensor_id")
-	if id == "" {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor for id %v not found", id)})
-	} else {
-		resp := make(map[string]string)
-		resp["message"] = fmt.Sprintf("Found sensor %s", id)
-
-		c.JSON(http.StatusOK, resp)
+		return
 	}
+
+	sensor, err := s.db.GetSensorById(c, int32(id))
+	resp := gin.H{
+		"message": fmt.Sprintf("Found sensor %d", sensor.ID),
+		"data":    sensor,
+	}
+
+	c.JSON(http.StatusOK, resp)
 }
 
 func (s *Server) CreateSensorHandler(c *gin.Context) {
