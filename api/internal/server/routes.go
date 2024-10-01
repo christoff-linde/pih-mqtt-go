01cc43a8eb7124523cf832b5cbfbc167bcd207f5
christoff-linde <christoff.linde@gmail.com>
2024-10-01 22:31:26 +0200

    feat(api): implement CreateSensor & DeleteSensor routes

--- api/internal/server/routes.go
+++ api/internal/server/routes.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	db "github.com/christoff-linde/pih-core-go/api/database"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -66,12 +68,41 @@ func (s *Server) GetSensorByIdHandler(c *gin.Context) {
 }
 
 func (s *Server) CreateSensorHandler(c *gin.Context) {
-	var sensor Sensor
-	if c.ShouldBind(&sensor) == nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("invalid body: %v", &sensor)})
-	} else {
-		c.JSON(http.StatusCreated, gin.H{"message": fmt.Sprintf("created sensor: %v", &sensor)})
+	var sensorData db.CreateSensorParams
+	if err := c.BindJSON(&sensorData); err != nil {
+		resp := gin.H{
+			"message": "Invalid input",
+			"error":   err,
+		}
+		c.JSON(http.StatusBadRequest, resp)
+		return
 	}
+
+	sensor, err := s.db.CreateSensor(c, db.CreateSensorParams{
+		SensorName: sensorData.SensorName,
+		CreatedAt: pgtype.Timestamptz{
+			Time:  time.Now(),
+			Valid: true,
+		},
+		UpdatedAt: pgtype.Timestamptz{
+			Time:  time.Now(),
+			Valid: true,
+		},
+	})
+	if err != nil {
+		resp := gin.H{
+			"message": "Failed to create Sensor",
+			"error":   err,
+		}
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	resp := gin.H{
+		"message": fmt.Sprintf("Created sensor %v", sensor.SensorName),
+		"data":    sensor,
+	}
+	c.JSON(http.StatusCreated, resp)
 }
 
 func (s *Server) UpdateSensorHandler(c *gin.Context) {
@@ -79,7 +110,37 @@ func (s *Server) UpdateSensorHandler(c *gin.Context) {
 }
 
 func (s *Server) DeleteSensorHandler(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{"message": "Not implemented"})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor for id %v not found", id)})
+		return
+	}
+
+	sensor, err := s.db.DeleteSensor(c, int32(id))
+
+	if sensor.RowsAffected() == 0 {
+		// TODO: improve error handling
+		resp := gin.H{
+			"message": "Failed to delete sensor",
+			"error":   fmt.Errorf("Sensor with id %v does not exist", id),
+		}
+		c.JSON(http.StatusBadRequest, resp)
+		return
+	}
+	if err != nil {
+		resp := gin.H{
+			"message": "Failed to delete sensor",
+			"error":   err,
+		}
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	resp := gin.H{
+		"message": fmt.Sprintf("Deleted sensor %v", id),
+		"data":    sensor,
+	}
+	c.JSON(http.StatusOK, resp)
 }
 
 func (s *Server) YeetHandler(c *gin.Context) {
