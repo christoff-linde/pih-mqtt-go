c3c874415de49c20f878c6befe814bee96135e62
christoff-linde <christoff.linde@gmail.com>
2024-10-07 19:45:39 +0200

    feat(api): implement sensor_metadata routes

--- api/internal/server/models.go
+++ api/internal/server/models.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"time"
+
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Sensor struct {
@@ -9,4 +11,14 @@ type Sensor struct {
 	UpdatedAt  time.Time `json:"updated_at"`
 	SensorName string    `json:"sensor_name"`
 	ID         int32     `json:"id,omitempty"`
+}
+
+type SensorMetadata struct {
+	ID             int32       `json:"id"`
+	SensorID       int32       `json:"sensor_id"`
+	SensorType     pgtype.Text `json:"sensor_type"`
+	Manufacturer   pgtype.Text `json:"manufacturer"`
+	ModelNumber    pgtype.Text `json:"model_number"`
+	SensorLocation pgtype.Text `json:"sensor_location"`
+	AdditionalData []byte      `json:"additional_data"`
 }
--- api/internal/server/routes.go
+++ api/internal/server/routes.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
 	db "github.com/christoff-linde/pih-core-go/api/database"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -23,15 +21,24 @@ func (s *Server) RegisterRoutes() http.Handler {
 		sensors.PUT("/:id", s.UpdateSensorHandler)
 		sensors.DELETE("/:id", s.DeleteSensorHandler)
 	}
+
+	sensor_metadata := v1.Group("/sensor_metadata")
+	{
+		sensor_metadata.GET("/", s.GetSensorMetadataHandler)
+		sensor_metadata.GET("/:id", s.GetSensorMetadataByIdHandler)
+		sensor_metadata.POST("/", s.CreateSensorMetadataHandler)
+		sensor_metadata.PUT("/:id", s.UpdateSensorMetadataHandler)
+		sensor_metadata.DELETE("/:id", s.DeleteSensorMetdataHandler)
+	}
 	return r
 }
 
-func (s *Server) GetSensorsHandler(c *gin.Context) {
+func (s *Server) GetSensorMetadataHandler(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "20"))
 	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
 	nextOffset := offset + limit
 
-	sensors, err := s.db.GetSensors(c, db.GetSensorsParams{
+	sensor_metadata, err := s.db.GetSensorMetadata(c, db.GetSensorMetadataParams{
 		Limit:  int32(limit),
 		Offset: int32(offset),
 	})
@@ -39,37 +46,42 @@ func (s *Server) GetSensorsHandler(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
-	if sensors == nil {
+	if sensor_metadata == nil {
 		nextOffset = 0
 	}
 
 	resp := gin.H{
-		"message":   fmt.Sprintf("Found %d sensors", len(sensors)),
-		"data":      sensors,
+		"message":   fmt.Sprintf("Found %d sensor_metadata", len(sensor_metadata)),
+		"data":      sensor_metadata,
 		"next_page": nextOffset,
 	}
 	c.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) GetSensorByIdHandler(c *gin.Context) {
+func (s *Server) GetSensorMetadataByIdHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor for id %v not found", id)})
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprint("Invalid sensor_id parameter", err)})
 		return
 	}
 
-	sensor, err := s.db.GetSensorById(c, int32(id))
+	sensor_metadata, err := s.db.GetSensorMetadataForSensorId(c, int32(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor Metadata for sensor_id %v not found", id)})
+		return
+	}
 	resp := gin.H{
-		"message": fmt.Sprintf("Found sensor %d", sensor.ID),
-		"data":    sensor,
+		"message": fmt.Sprintf("Found sensor_metadata %d", sensor_metadata.ID),
+		"data":    sensor_metadata,
 	}
 
 	c.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) CreateSensorHandler(c *gin.Context) {
-	var sensorData db.CreateSensorParams
-	if err := c.BindJSON(&sensorData); err != nil {
+func (s *Server) CreateSensorMetadataHandler(c *gin.Context) {
+	var sensorMetadata db.CreateSensorMetadataParams
+
+	if err := c.BindJSON(&sensorMetadata); err != nil {
 		resp := gin.H{
 			"message": "Invalid input",
 			"error":   err,
@@ -78,20 +90,17 @@ func (s *Server) CreateSensorHandler(c *gin.Context) {
 		return
 	}
 
-	sensor, err := s.db.CreateSensor(c, db.CreateSensorParams{
-		SensorName: sensorData.SensorName,
-		CreatedAt: pgtype.Timestamptz{
-			Time:  time.Now(),
-			Valid: true,
-		},
-		UpdatedAt: pgtype.Timestamptz{
-			Time:  time.Now(),
-			Valid: true,
-		},
+	sensor_metadata, err := s.db.CreateSensorMetadata(c, db.CreateSensorMetadataParams{
+		SensorID:       sensorMetadata.SensorID,
+		SensorType:     sensorMetadata.SensorType,
+		Manufacturer:   sensorMetadata.Manufacturer,
+		ModelNumber:    sensorMetadata.ModelNumber,
+		SensorLocation: sensorMetadata.SensorLocation,
+		AdditionalData: sensorMetadata.AdditionalData,
 	})
 	if err != nil {
 		resp := gin.H{
-			"message": "Failed to create Sensor",
+			"message": "Failed to create Sensor Metadata",
 			"error":   err,
 		}
 		c.JSON(http.StatusInternalServerError, resp)
@@ -99,16 +108,16 @@ func (s *Server) CreateSensorHandler(c *gin.Context) {
 	}
 
 	resp := gin.H{
-		"message": fmt.Sprintf("Created sensor %v", sensor.SensorName),
-		"data":    sensor,
+		"message": fmt.Sprintf("Created sensor_metadata %v for sensor_id %v", sensor_metadata.ID, sensorMetadata.SensorID),
+		"data":    sensor_metadata,
 	}
 	c.JSON(http.StatusCreated, resp)
 }
 
-func (s *Server) UpdateSensorHandler(c *gin.Context) {
+func (s *Server) UpdateSensorMetadataHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
-	var sensorData db.UpdateSensorParams
-	if err := c.BindJSON(&sensorData); err != nil {
+	var sensorMetadata db.UpdateSensorMetadataParams
+	if err := c.BindJSON(&sensorMetadata); err != nil {
 		resp := gin.H{
 			"message": "Invalid input",
 			"error":   err,
@@ -117,23 +126,27 @@ func (s *Server) UpdateSensorHandler(c *gin.Context) {
 		return
 	}
 
-	sensor, err := s.db.UpdateSensor(c, db.UpdateSensorParams{
-		ID:         sensorData.ID,
-		SensorName: sensorData.SensorName,
+	sensor_metadata, err := s.db.UpdateSensorMetadata(c, db.UpdateSensorMetadataParams{
+		SensorID:       sensorMetadata.SensorID,
+		SensorType:     sensorMetadata.SensorType,
+		Manufacturer:   sensorMetadata.Manufacturer,
+		ModelNumber:    sensorMetadata.ModelNumber,
+		SensorLocation: sensorMetadata.SensorLocation,
+		AdditionalData: sensorMetadata.AdditionalData,
 	})
 
-	if sensor.RowsAffected() == 0 {
+	if sensor_metadata.RowsAffected() == 0 {
 		// TODO: improve error handling
 		resp := gin.H{
-			"message": "Failed to update sensor",
-			"error":   fmt.Errorf("Sensor with id %v does not exist", id),
+			"message": "Failed to update sensor_metadata",
+			"error":   fmt.Errorf("Sensor_metadata with id %v or sensor_id %v does not exist", id, sensorMetadata.SensorID),
 		}
 		c.JSON(http.StatusBadRequest, resp)
 		return
 	}
 	if err != nil {
 		resp := gin.H{
-			"message": "Failed to update sensor",
+			"message": "Failed to update sensor_metadata",
 			"error":   err,
 		}
 		c.JSON(http.StatusInternalServerError, resp)
@@ -141,33 +154,33 @@ func (s *Server) UpdateSensorHandler(c *gin.Context) {
 	}
 
 	resp := gin.H{
-		"message": fmt.Sprintf("Updated sensor %v", id),
-		"data":    sensor,
+		"message": fmt.Sprintf("Updated sensor_metadata %v", id),
+		"data":    sensor_metadata,
 	}
 	c.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) DeleteSensorHandler(c *gin.Context) {
+func (s *Server) DeleteSensorMetdataHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor for id %v not found", id)})
+		c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Sensor Metadata for id %v not found", id)})
 		return
 	}
 
-	sensor, err := s.db.DeleteSensor(c, int32(id))
+	sensor_metadata, err := s.db.DeleteSensorMetadata(c, int32(id))
 
-	if sensor.RowsAffected() == 0 {
+	if sensor_metadata.RowsAffected() == 0 {
 		// TODO: improve error handling
 		resp := gin.H{
-			"message": "Failed to delete sensor",
-			"error":   fmt.Errorf("Sensor with id %v does not exist", id),
+			"message": "Failed to delete sensor_metadata",
+			"error":   fmt.Errorf("Sensor Metadtata with id %v does not exist", id),
 		}
 		c.JSON(http.StatusBadRequest, resp)
 		return
 	}
 	if err != nil {
 		resp := gin.H{
-			"message": "Failed to delete sensor",
+			"message": "Failed to delete sensor_metadata",
 			"error":   err,
 		}
 		c.JSON(http.StatusInternalServerError, resp)
@@ -175,8 +188,8 @@ func (s *Server) DeleteSensorHandler(c *gin.Context) {
 	}
 
 	resp := gin.H{
-		"message": fmt.Sprintf("Deleted sensor %v", id),
-		"data":    sensor,
+		"message": fmt.Sprintf("Deleted sensor_metadata %v", id),
+		"data":    sensor_metadata,
 	}
 	c.JSON(http.StatusOK, resp)
 }
